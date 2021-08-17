82fa8eab049fa6c4aae3907cea36c9045f3ce142
AdamSchuller <88639701+AdamSchuller@users.noreply.github.com>
2021-08-16 20:18:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ cpu = pd.read_csv('CPU Ranks.csv')
 
 gpu = pd.read_csv('GPU_ranks.csv')
 gpu = gpu.drop(columns=['Unnamed: 5', 'Unnamed: 6'])
+########New Code added 8-16-2021
+
+gpu = gpu[(gpu['Videocard Name'] != 'Videocard Name')]
+gpu.dropna(subset=['Videocard Name'], inplace = True)
+
+gpu['Passmark G3D Mark'] = pd.to_numeric(gpu['Passmark G3D Mark'])
+gpu['Rank'] = [pd.to_numeric(str(gpu['Rank'].iloc[x])) if gpu['Rank'].iloc[x] == gpu['Rank'].iloc[x] else np.nan for x in range(len(gpu['Rank']))]
+gpu['Videocard Value'] = [pd.to_numeric(str(gpu['Videocard Value'].iloc[x])) if gpu['Videocard Value'].iloc[x] == gpu['Videocard Value'].iloc[x] else np.nan for x in range(len(gpu['Videocard Value']))]
+gpu['usd'] = [pd.to_numeric(str(gpu['usd'].iloc[x]).replace('$','').replace('*','').replace(',','').replace('Price','').replace('(USD)','')) if gpu['usd'].iloc[x] == gpu['usd'].iloc[x] else np.nan for x in range(len(gpu['usd']))]
+
+cpu['Price (USD)'] = [pd.to_numeric(str(cpu['Price (USD)'].iloc[x]).replace('$','').replace('*','').replace(',','').replace('Price','').replace('(USD)','')) if cpu['Price (USD)'].iloc[x] == cpu['Price (USD)'].iloc[x] else np.nan for x in range(len(cpu['Price (USD)']))]
+cpu['CPU Mark (Higher is better)'] = [pd.to_numeric(str(cpu['CPU Mark (Higher is better)'].iloc[x]).replace(',','')) if cpu['CPU Mark (Higher is better)'].iloc[x] == cpu['CPU Mark (Higher is better)'].iloc[x] else np.nan for x in range(len(cpu['CPU Mark (Higher is better)']))]
+cpu['CPU Value (Higher is better)'] = [pd.to_numeric(str(cpu['CPU Value (Higher is better)'].iloc[x])) if cpu['CPU Value (Higher is better)'].iloc[x] == cpu['CPU Value (Higher is better)'].iloc[x] else np.nan for x in range(len(cpu['CPU Value (Higher is better)']))]
